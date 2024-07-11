5c69155ddb753926c1f3c6c83a4f38d29793fbaa
Mohamed eltabey <hamom@M10>
2024-07-11 13:31:00 +0300

    Add project files.

---
+++ ITI_Training/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.ExceptionServices;
+using System.Timers;
+
+namespace ITI_Training
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= 12; i++)
+            {
+                Console.WriteLine($"{num} * {i} = {num * i}");
+            }
+        }
+    }
+}
